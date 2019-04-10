c5cb4bb4ebfab744d4bf4c6c9788d9981bb6802d
Richsea <201402377@cs-cnu.org>
2019-04-11 02:19:39 +0900

    fix error & finish sum

--- Node_Max_Sun/Node_Max_Sum.java
+++ Node_Max_Sun/Node_Max_Sum.java
@@ -8,7 +8,6 @@ public class Node_Max_Sum {
 	public static int max(Node node)
 	{
 		int max = Integer.MIN_VALUE;
-		Node start = node;
 		IntNode intNode = null;
 		ListNode listNode = null;
 		
@@ -16,17 +15,13 @@ public class Node_Max_Sum {
 		{
 			listNode = (ListNode) node;
 			
-			if(listNode.value.getClass() != null)
+			if(listNode.value != null)
 			{
 				int temp = max(listNode.value);
 				max = Math.max(temp, max);
 			}
 			
-			if(listNode.getNext() == null)
-			{
-				return max;
-			}
-			else
+			if(listNode.getNext() != null)
 			{
 				int temp = max(listNode.getNext());
 				max = Math.max(temp, max);
@@ -38,14 +33,10 @@ public class Node_Max_Sum {
 			
 			max = intNode.value;
 			
-			if(intNode.getNext() == null)
-			{
-				return max;
-			}
-			else
+			if(intNode.getNext() != null)
 			{
 				int temp = max(intNode.getNext());
-				max = Math.max(max, temp);				
+				max = Math.max(max, temp);
 			}
 		}		
 		
@@ -54,15 +45,44 @@ public class Node_Max_Sum {
 	
 	public static int sum(Node node)
 	{
-		return 0;
+		IntNode intNode = null;
+		ListNode listNode = null;
+		int totalValue = 0;
+		
+		if(node.getClass() == ListNode.class)
+		{
+			listNode = (ListNode) node;
+			
+			if(listNode.value != null)
+			{
+				totalValue += sum(listNode.value);
+			}
+			
+			if(listNode.getNext() != null)
+			{
+				totalValue += sum(listNode.getNext());
+			}
+		}
+		else if(node.getClass() == IntNode.class)
+		{
+			intNode = (IntNode) node;
+			totalValue = intNode.value;
+			
+			if(intNode.getNext() != null)
+			{
+				totalValue += sum(intNode.getNext());
+			}
+		}
+		
+		return totalValue;
 	}
 	
 	public static void main(String[] args)
 	{
-		Node node = TreeFactory.createtTree("( 3 ( 5 2 3 ) -378 )");
-		//Node node = TreeFactory.createtTree("( ( 3 ( ( 10 ) ) 6 ) 4 1 ( ) -2 ( ) )");
+		Node node = TreeFactory.createtTree("( ( 3 ( ( 10 ) ) 6 ) 4 1 ( ) -2 ( ) )");
 		
 		System.out.println(max(node));
+		System.out.println(sum(node));
 		
 	}
 }
